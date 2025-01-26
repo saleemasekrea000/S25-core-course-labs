4ffb28fa106e80a67fed3edcee90ad4ac73ca3a3
saleemasekrea000 <s.asekrea@innopolis.university>
2025-01-26 15:12:30 +0300

    Completed Task 1: Python Web Application

---
+++ app_python/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+
+from app_python.app_service import get_time
+
+app = FastAPI()
+
+
+@app.get("/",response_class=HTMLResponse)
+def show_time(request:Request):
+    return get_time(request)
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ app_python/app_service.py
@@ -0,0 +1,20 @@
+
+from datetime import datetime
+import pytz
+
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+templates = Jinja2Templates(directory="app_python")
+
+
+def get_time(request:Request, timezone: str = "Europe/Moscow") -> str:
+    
+    return templates.TemplateResponse("time_template.html", {"request": request,"current_time": get_tz_time(timezone)})
+
+
+
+
+def get_tz_time(timezone) -> str:
+    
+    return datetime.now(pytz.timezone(timezone)).strftime("%H:%M (%I:%M %p)")
